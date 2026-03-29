36dbebced93e9870e3011d0dc76830fd6195f873
Alperen Aykutlu <alperenaykutlu@users.noreply.github.com>
2026-03-29 15:25:24 +0300

    chore: Finalize backend configurations

--- social/eslint.config.js
+++ social/eslint.config.js
@@ -1,10 +1,10 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import { defineConfig } from 'vitest/config';
 
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
+export default defineConfig({
+  test: {
+    env: {
+      JWT_SECRET: 'test-secret-key',
+      JWT_REFRESH_SECRET: 'test-refresh-secret',
+    },
   },
-]);
+});
